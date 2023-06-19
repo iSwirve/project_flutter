8a4e33af993bf3ce140f29e90da0b7b5bf962f7a
HaoD2 <73094389+HaoD2@users.noreply.github.com>
2023-06-19 13:46:10 +0700

    memo untuk detail dan insert
    selesai untuk update dan delete kek nya gak usah cmn klo mu ditambah gpp

--- lib/pages/memo/memo_detail.dart
+++ lib/pages/memo/memo_detail.dart
@@ -1,8 +1,19 @@
+import 'package:basicpos_v2/pages/memo/memo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:focused_menu/modals.dart';
+import 'package:basicpos_v2/constants/urls.dart' as url;
+import 'package:basicpos_v2/constants/colors.dart' as colors;
+
+import '../../components/custom_text.dart';
 
 class memo_detail extends StatefulWidget {
-  const memo_detail({super.key, required int index});
+  final index;
+
+  const memo_detail({super.key, this.index});
   static const routeName = '/memo_detail';
 
   @override
@@ -10,8 +21,114 @@ class memo_detail extends StatefulWidget {
 }
 
 class _memo_detailState extends State<memo_detail> {
+    int count = 0;
+    final CollectionReference _memo =
+      FirebaseFirestore.instance.collection('Memo');
+
+  final CollectionReference _barang =
+      FirebaseFirestore.instance.collection('Barang');
+
+
+
+
+
+  getdata() async {
+    QuerySnapshot querySnapshot = await _barang.get();
+    final dataBarang = querySnapshot.docs.map((doc) => doc.data()).toList();
+    QuerySnapshot qs = await _memo.get();
+    final dataMemo = qs.docs.map((doc) => doc.data()).toList();
+    return [dataMemo, dataBarang];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text(
+          "Detail barang",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black87),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => memo(),
+              ),
+            );
+          },
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: FutureBuilder<dynamic>(
+        initialData: [],
+        future: getdata(),
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (!snapshot.hasData ||
+              snapshot.data == null ||
+              snapshot.data.isEmpty ||
+              snapshot.hasError) {
+            if (count > 0) {
+              count = 0;
+              return Container();
+            } else {
+              return Container(
+                height: MediaQuery.of(context).size.height - 200,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+          } else {
+            // print(snapshot.data.runtimeType);
+            var judul_memo =
+                snapshot.data[0][widget.index]["judul_memo"].toString();
+            var nama_barang =
+                snapshot.data[1][widget.index]["nama_barang"].toString();
+            var catatan = snapshot.data[0][widget.index]["catatan"].toString();
+
+            var qty =
+                snapshot.data[0][widget.index]["qty"].toString();
+
+            return SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.only(top: 10, left: 20),
+                child: Column(
+                  children: [
+                    CustomText(
+                      text: "Judul Memo : $judul_memo",
+                      textStyle: TextStyle(fontSize: 12),
+                      sizedBox: SizedBox(height: 5),
+                    ),
+                    CustomText(
+                      text: "Nama Barang : $nama_barang",
+                      textStyle: TextStyle(fontSize: 12),
+                      sizedBox: SizedBox(height: 5),
+                    ),
+                    CustomText(
+                      text: "Catatan Memo : $catatan",
+                      textStyle: TextStyle(fontSize: 12),
+                      sizedBox: SizedBox(height: 5),
+                    ),
+
+                    CustomText(
+                      text: "Kuantiti : $qty",
+                      textStyle: TextStyle(fontSize: 12),
+                      sizedBox: SizedBox(height: 5),
+                    ),
+
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
+      backgroundColor: Colors.white,
+    );
   }
-}
+  }
