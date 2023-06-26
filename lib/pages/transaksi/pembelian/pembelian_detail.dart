d570cd5390067d40c47f47f19c12318986b19770
HaoD2 <73094389+HaoD2@users.noreply.github.com>
2023-06-26 18:26:37 +0700

    nambah halaman tambah aja

---
+++ lib/pages/transaksi/pembelian/pembelian_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class pembelian_detail extends StatefulWidget {
+    final index;
+  const pembelian_detail({super.key, this.index});
+
+  @override
+  State<pembelian_detail> createState() => _pembelian_detailState();
+}
+
+class _pembelian_detailState extends State<pembelian_detail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
